ba1c117dd5c96e5abf31df32b11a0e9407891018
mariaelizasa <eliza.sads@gmail.com>
2021-07-29 22:01:01 -0300

    created db.clj and customer hashmaps

--- src/credit_card/core.clj
+++ src/credit_card/core.clj
@@ -1,6 +1,24 @@
-(ns credit-card.core)
+(ns credit-card.core
+  (:require [credit-card.db]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+(def random-cvv-generator (* (rand-int 10) 100))
+(def random-card-number-generator (* (rand-int 1000) 1000))
+
+for
+(defn create-client-data
+  [name cpf email]
+
+  {:id cpf
+   :data {:name name
+          :cpf cpf
+          :email email}
+
+   :credit-card {:number random-card-number-generator
+                 :initial-limit 400
+                 :validate 10
+                 :cvv random-cvv-generator}})
+
+(def client (create-client-data "Maria" 920 "maria@gmail.com"))
+
+
---
+++ src/credit_card/db.clj
@@ -0,0 +1,54 @@
+(ns credit-card.db)
+
+(def all-orders {{:date       1
+            :price       (rand-int 1000)
+             :commerce   "Mc Donalds"
+             :categoria  "Food"}
+            
+            {:date        2             
+             :price       (rand-int 1000)
+             :commerce    "Bolovo"
+             :categoria   "Locker Room"}
+            
+            {:date        3
+             :price       (rand-int 1000)
+             :commerce    "Nike"
+             :categoria   "Locker Room"}
+            
+            {:date        824
+             :price       (rand-int 1000)
+             :commerce    "Burguer King"
+             :categoria   "Food"}
+             
+            {:date        63
+             :price       (rand-int 1000)
+             :commerce    "Riot Games"
+             :categoria   "Leisure"}
+            
+            {:date        5
+             :price       (rand-int 1000)
+             :commerce    "Sushi"
+             :categoria   "Food"}
+            
+            {:date        6
+             :price       (rand-int 1000)
+             :commerce    "Burguer King"
+             :categoria   "Food"}
+            
+            {:date        8
+             :price       (rand-int 1000)
+             :commerce    "Family Park"
+             :categoria   "Leisure"}
+            
+            {:date        9
+             :price       (rand-int 1000)
+             :commerce    "Spoletto"
+             :categoria   "Food"}
+            
+            {:date        24
+             :price       (rand-int 1000)
+             :commerce    "Adidas"
+             :categoria   "Locker Room"}}
+  
+  )
+
